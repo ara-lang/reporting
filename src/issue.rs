be9ffaf118c0833a8d352bd835947019ba0c43d9
Kennedy Tedesco <kennedyt.tw@gmail.com>
2023-01-06 21:06:45 -0300

    new: added severity() method to Report

--- src/issue.rs
+++ src/issue.rs
@@ -4,7 +4,7 @@ use serde::Serialize;
 
 use crate::annotation::Annotation;
 
-#[derive(Debug, PartialEq, Eq, Clone, Deserialize, Serialize, JsonSchema)]
+#[derive(Debug, PartialEq, Eq, Ord, Clone, PartialOrd, Deserialize, Serialize, JsonSchema)]
 #[serde(rename_all = "snake_case", tag = "type")]
 pub enum IssueSeverity {
     Error,
