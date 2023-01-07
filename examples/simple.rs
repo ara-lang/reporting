5839ac3c783b04c480bbea2ffa217778788fef40
Kennedy Tedesco <kennedyt.tw@gmail.com>
2023-01-07 00:26:20 -0300

    chore: use new method in test

--- examples/simple.rs
+++ examples/simple.rs
@@ -21,28 +21,30 @@ $b = match $a {
 
     let map = SourceMap::new(vec![Source::new(SourceKind::Script, origin, code)]);
 
-    let report = Report::new().with_issue(
-        Issue::error(
-            "E0417",
-            "`match` arms have incompatible types",
-            origin,
-            6,
-            67,
+    let report = Report::new()
+        .with_issue(
+            Issue::error(
+                "E0417",
+                "`match` arms have incompatible types",
+                origin,
+                6,
+                67,
+            )
+            .with_annotation(
+                Annotation::secondary(origin, 26, 27)
+                    .with_message("this is found to be of type `{int}`"),
+            )
+            .with_annotation(
+                Annotation::secondary(origin, 38, 39)
+                    .with_message("this is found to be of type `{int}`"),
+            )
+            .with_annotation(
+                Annotation::secondary(origin, 56, 64)
+                    .with_message("expected `{int}`, found `{string}`"),
+            )
+            .with_note("for more information about this error, try `ara --explain E0417`"),
         )
-        .with_annotation(
-            Annotation::secondary(origin, 26, 27)
-                .with_message("this is found to be of type `{int}`"),
-        )
-        .with_annotation(
-            Annotation::secondary(origin, 38, 39)
-                .with_message("this is found to be of type `{int}`"),
-        )
-        .with_annotation(
-            Annotation::secondary(origin, 56, 64)
-                .with_message("expected `{int}`, found `{string}`"),
-        )
-        .with_note("for more information about this error, try `ara --explain E0417`"),
-    );
+        .with_message("This is a report message");
 
     let builder = ReportBuilder::new(&map, report)
         .with_colors(ColorChoice::Always)
